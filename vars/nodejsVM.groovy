f243c6460091c05d3464ef321bac50a87922749d
ramana11k <ramana11k@gmail.com>
2024-03-26 15:32:24 +0530

    jenkins

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -1,87 +1,37 @@
 def call(Map configMap) {
-pipeline {
-    agent {
-        node {
-            label 'AGENT-1'
-        }
-    }
-
-     environment { 
-        packageVersion = ''
-        //can be maitained in pipeline globals
-       // nexusURL = '172.31.93.195:8081'
-    }
-
-    options {
-        ansiColor('xterm')
-        timeout(time: 1, unit: 'HOURS') 
-        disableConcurrentBuilds() //It wont allow two builds at a time
-    }
-
-
-    parameters {
-    
-            booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
-    
-    // //     string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
-    // //     text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')    
-    // //     choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
-    // //     password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
-    }
-   
-
-   
-    //BUILD
-    stages {       
-        
-        stage('Get the version') {
-            steps {
-                script {
-                    def packageJson = readJSON file: 'package.json'
-                    packageVersion = packageJson.version
-                    echo "application version: $packageVersion"
-                }
-                    
-            }
-        }
-
-        stage('Install dependencies') {
-            steps {                
-                sh """
-                        npm install
-                """                                  
-            }
-        }
-
-        def call(Map configMap){
     pipeline {
         agent {
             node {
                 label 'AGENT-1'
             }
         }
+
         environment { 
             packageVersion = ''
-            // can maintain in pipeline globals
-            //nexusURL = '172.31.5.95:8081'
+            //can be maitained in pipeline globals
+        // nexusURL = '172.31.93.195:8081'
         }
+
         options {
-            timeout(time: 1, unit: 'HOURS')
-            disableConcurrentBuilds()
+            ansiColor('xterm')
+            timeout(time: 1, unit: 'HOURS') 
+            disableConcurrentBuilds() //It wont allow two builds at a time
         }
+
         parameters {
-            // string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+        
+                booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
+        
+        // //     string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+        // //     text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')    
+        // //     choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+        // //     password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+        } 
 
-            // text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
-
-            booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
-
-            // choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
-
-            // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
-        }
-        // build
-        stages {
+    
+        //BUILD
+        stages {       
+            
             stage('Get the version') {
                 steps {
                     script {
@@ -89,13 +39,15 @@ pipeline {
                         packageVersion = packageJson.version
                         echo "application version: $packageVersion"
                     }
+                        
                 }
             }
+
             stage('Install dependencies') {
-                steps {
+                steps {                
                     sh """
                         npm install
-                    """
+                    """                                  
                 }
             }
             stage('Unit tests') {
@@ -158,22 +110,18 @@ pipeline {
                 }
             }
         }
-        // post build
-        post { 
-            always { 
-                echo 'I will always say Hello again!'
-                deleteDir()
+            // post build
+            post { 
+                always { 
+                    echo 'I will always say Hello again!'
+                    deleteDir()
+                }
+                failure { 
+                    echo 'this runs when pipeline is failed, used generally to send some alerts'
+                }
+                success{
+                    echo 'I will say Hello when pipeline is success'
+                }
             }
-            failure { 
-                echo 'this runs when pipeline is failed, used generally to send some alerts'
-            }
-            success{
-                echo 'I will say Hello when pipeline is success'
-            }
-        }
     }
-}
-  }
-}                
-   
-}
+} 
